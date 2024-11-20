ebbf2fe1b6f7f64aaf7c3c24f26eb69983797cec
Rama Aryo <ramaprambudi@gmail.com>
2024-11-20 09:36:30 +0700

    first init

---
+++ src/models/userModels.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "users",
+  }
+);
+
+module.exports = User;
---
+++ test/src/server.js
@@ -0,0 +1,37 @@
+const Hapi = require("@hapi/hapi");
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: "localhost",
+  });
+
+  await server.register(require("@hapi/jwt"));
+
+  server.auth.strategy("jwt", "jwt", {
+    keys: process.env.JWT_SECRET,
+    verify: {
+      aud: "urn:audience:users",
+      iss: "urn:issuer:api",
+      sub: false,
+    },
+    validate: (artifacts, request, h) => {
+      return {
+        isValid: true,
+        credentials: { user: artifacts.decoded.payload.user },
+      };
+    },
+  });
+
+  server.auth.default("jwt");
+
+  // Registrasi rute
+  userRoutes(server); // Cara yang benar untuk mendaftarkan rute Anda
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
